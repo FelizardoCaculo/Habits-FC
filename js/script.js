0a8c2ac4ef0eb4e5bd5833bfd3f0d6299a35614f
FelizardoCaculo <felizardo1987@gmail.com>
2023-01-22 20:44:31 +0100

    Created js new functions and local storage do the project

--- js/script.js
+++ js/script.js
@@ -1,11 +1,30 @@
 const form = document.querySelector('#form-habits');
 const nlwSetup = new NLWSetup(form);
+const button = document.querySelector('header button');
+button.addEventListener('click', add);
+form.addEventListener('change', save);
 
-const data = {
-    run: ['01-01', '01-02', '01-06', '01-07', '01-08'],
-    takePills: ['01-03'],
-    journal: ['01-02']
+
+function add() {
+    const today = new Date().toLocaleDateString('pt-pt').slice(0, -5);
+    const dayExists = nlwSetup.dayExists(today);
+
+    if (dayExists === true) {
+        alert('Atenção! este dia ja foi resistado 🚫');
+        return;
+    } else {
+        alert('Dia registado com sucesso ✅');
+    }
+
+    nlwSetup.addDay(today);
 }
+
+function save() {
+    localStorage.setItem('NLWSetup@habits', JSON.stringify(nlwSetup.data));
+}
+
+const data = JSON.parse(localStorage.getItem('NLWSetup@habits')) || {};
+console.log(data)
 
 nlwSetup.setData(data);
 nlwSetup.load();
